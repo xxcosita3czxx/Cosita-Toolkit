9d42c724f4c27a494f0c6e9d9c87e28dcbaa64fc
xxcosita3czxx <kokosove18@gmail.com>
2023-06-13 21:29:44 +0200

    adding more tests

--- _test.py
+++ _test.py
@@ -3,3 +3,5 @@ import cosita_toolkit as costk
 def test_gitapi():
     costk.github_api.get_last_info_raw("xxcosita3czxx")
     costk.github_api.get_info_usr("xxcosita3czxx")
+def test_osint():
+    costk.osint_framework.universal.check_username("cosita3ct","Github")
