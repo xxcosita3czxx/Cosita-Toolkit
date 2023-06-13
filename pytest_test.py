821c8868969cc0e60357ac8e4149b4bac4a5e50a
xxcosita3czxx <kokosove18@gmail.com>
2023-06-13 21:35:28 +0200

    adding all tests i need

--- pytest_test.py
+++ pytest_test.py
@@ -5,3 +5,7 @@ def test_gitapi():
     costk.github_api.get_info_usr("xxcosita3czxx")
 def test_osint():
     costk.osint_framework.universal.check_username("cosita3ct","Github")
+def test_pokeapi():
+    costk.PokeAPI.get_pokemon_raw("pikachu")
+def test_main():
+    costk.main()
