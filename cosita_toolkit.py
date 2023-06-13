eb9c1de59c80675554b7bac571b636375b23157b
xxcosita3czxx <kokosove18@gmail.com>
2023-06-13 21:01:19 +0200

    error handling

--- cosita_toolkit.py
+++ cosita_toolkit.py
@@ -1,10 +1,11 @@
 import ctypes
-import win32gui
+import platform
+if platform.system() == "Windows":
+    import win32gui
 import psutil
 import requests
 import json
 from time import gmtime, strftime
-import pytest
 ## variables needed for code to work
 LICENSE = """
 MIT License
@@ -183,55 +184,64 @@ def main():
 # windows memory editor
 class memMod:
     def pid_by_name(target_string,exe_name):
-        for proc in psutil.process_iter(['pid', 'name', 'create_time']):
-            try:
-                hwnds = []
-                # Enumerate all windows and add the handle to the list if the target string is in the title
-                def callback(hwnd, hwnds):
-                    if win32gui.IsWindowVisible(hwnd):
-                        title = win32gui.GetWindowText(hwnd)
-                        if target_string in title:
-                            hwnds.append(hwnd)
-                win32gui.EnumWindows(callback, hwnds)
-                # If we found a matching window, check the parent process
-                if hwnds:
-                    try:
-                        pid = proc.pid
-                        parent_pid = proc.ppid()
-                        parent_name = psutil.Process(parent_pid).name()
-                        exe_name = psutil.Process(proc.pid).exe()
-                        if proc.name() == exe_name:
-                            #print(f"Found process with window title containing {target_string} and PID {pid} and name: {exe_name}")
-                            return pid
-                    except psutil.AccessDenied:
-                # Access denied - ignore this process
-                        pass
-                    except psutil.NoSuchProcess:
-                # Process may have terminated while iterating
-                        pass
-            except:
-                pass
+        if platform.system() == "Windows":
+            for proc in psutil.process_iter(['pid', 'name', 'create_time']):
+                try:
+                    hwnds = []
+                    # Enumerate all windows and add the handle to the list if the target string is in the title
+                    def callback(hwnd, hwnds):
+                        if win32gui.IsWindowVisible(hwnd):
+                            title = win32gui.GetWindowText(hwnd)
+                            if target_string in title:
+                                hwnds.append(hwnd)
+                    win32gui.EnumWindows(callback, hwnds)
+                    # If we found a matching window, check the parent process
+                    if hwnds:
+                        try:
+                            pid = proc.pid
+                            parent_pid = proc.ppid()
+                            parent_name = psutil.Process(parent_pid).name()
+                            exe_name = psutil.Process(proc.pid).exe()
+                            if proc.name() == exe_name:
+                                #print(f"Found process with window title containing {target_string} and PID {pid} and name: {exe_name}")
+                                return pid
+                        except psutil.AccessDenied:
+                    # Access denied - ignore this process
+                            pass
+                        except psutil.NoSuchProcess:
+                    # Process may have terminated while iterating
+                            pass
+                except:
+                    pass
+            else:
+                print(f"No process found with window title containing {target_string}")
+                return None
         else:
-            print(f"No process found with window title containing {target_string}")
-            return None
+            return "Non-windows system detected! skipping..."
     def modify(pid, address, new_value):
-        new_value = ctypes.c_int(new_value)
-        process_handle = ctypes.windll.kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, pid)
-        buffer = ctypes.create_string_buffer(SIZEOF_INT)
-        bytes_read = ctypes.c_size_t(0)
-        ctypes.windll.kernel32.ReadProcessMemory(process_handle, address, buffer, SIZEOF_INT, ctypes.byref(bytes_read))
-        value = ctypes.c_int.from_buffer(buffer)
-        ctypes.windll.kernel32.WriteProcessMemory(process_handle, address, ctypes.byref(new_value), SIZEOF_INT, None)
-        ctypes.windll.kernel32.CloseHandle(process_handle)
-        return "OK"
+        if platform.system()=="Windows":
+            new_value = ctypes.c_int(new_value)
+            process_handle = ctypes.windll.kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, pid)
+            buffer = ctypes.create_string_buffer(SIZEOF_INT)
+            bytes_read = ctypes.c_size_t(0)
+            ctypes.windll.kernel32.ReadProcessMemory(process_handle, address, buffer, SIZEOF_INT, ctypes.byref(bytes_read))
+            value = ctypes.c_int.from_buffer(buffer)
+            ctypes.windll.kernel32.WriteProcessMemory(process_handle, address, ctypes.byref(new_value), SIZEOF_INT, None)
+            ctypes.windll.kernel32.CloseHandle(process_handle)
+            return "OK"
+        else:
+            return "Non-windows system detected! skipping..."
     def check(pid, address):
-        process_handle = ctypes.windll.kernel32.OpenProcess(PROCESS_VM_READ, False, pid)
-        buffer = ctypes.create_string_buffer(SIZEOF_INT)
-        bytes_read = ctypes.c_size_t(0)
-        ctypes.windll.kernel32.ReadProcessMemory(process_handle, address, buffer, SIZEOF_INT, ctypes.byref(bytes_read))
-        value = ctypes.c_int.from_buffer(buffer).value
-        ctypes.windll.kernel32.CloseHandle(process_handle)
-        return value
+        if platform.system()=="Windows":
+            process_handle = ctypes.windll.kernel32.OpenProcess(PROCESS_VM_READ, False, pid)
+            buffer = ctypes.create_string_buffer(SIZEOF_INT)
+            bytes_read = ctypes.c_size_t(0)
+            ctypes.windll.kernel32.ReadProcessMemory(process_handle, address, buffer, SIZEOF_INT, ctypes.byref(bytes_read))
+            value = ctypes.c_int.from_buffer(buffer).value
+            ctypes.windll.kernel32.CloseHandle(process_handle)
+            return value
+        else:
+            return "Non-windows system detected! skipping..."
 class github_api:
     def get_last_info_raw(name,save_place=None,file_name=None):
         url = f"https://api.github.com/users/{name}/events/public"
@@ -280,3 +290,5 @@ class osint_framework:
                 if service_name != "All":
                     break
             return results
+if __name__ == "__main__":
+    main()
