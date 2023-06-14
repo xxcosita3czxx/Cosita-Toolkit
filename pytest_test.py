1f9c6ebff8a1566cd5607ee6ff25ce81372a9d38
xxcosita3czxx <kokosove18@gmail.com>
2023-06-14 07:10:29 +0200

    fixin

---
+++ pytest_test.py
@@ -0,0 +1,10 @@
+import cosita_toolkit as costk
+def test_gitapi():
+    costk.github_api.get_last_info_raw("xxcosita3czxx")
+    costk.github_api.get_info_usr("xxcosita3czxx")
+def test_osint():
+    costk.osint_framework.universal.check_username("cosita3ct","Github")
+def test_pokeapi():
+    costk.PokeAPI.get_pokemon_raw("pikachu")
+def test_main():
+    costk.main()
