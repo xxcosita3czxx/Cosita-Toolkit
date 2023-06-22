02137af3d1509fa9ef4625f4804a3abb49e3b70c
xxcosita3czxx <kokosove18@gmail.com>
2023-06-22 15:33:43 +0200

    no more discord api tests

--- pytest_test.py
+++ pytest_test.py
@@ -8,6 +8,3 @@ def test_pokeapi():
     costk.PokeAPI.get_pokemon_raw("pikachu")
 def test_main():
     costk.main()
-def test_discord():
-    embed = costk.discord.embeds.embed("test","test")
-    costk.discord.embeds.add_field(embed,"test",1)
