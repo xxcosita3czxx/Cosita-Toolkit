2aff8bd98457fdda10e95044727f8e0e12b2a1c4
xxcosita3czxx <kokosove18@gmail.com>
2024-04-30 21:29:34 +0200

    pytests

--- pytest_test.py
+++ pytest_test.py
@@ -1,14 +1,12 @@
 import os
 import cosita_toolkit as costk
+
 def test_gitapi():
     costk.github_api.get_last_info_raw("xxcosita3czxx")
     costk.github_api.get_info_usr("xxcosita3czxx")
 def test_osint():
-    costk.osint_framework.universal.check_username("cosita3ct","Github")
+    costk.osint_framework.universal.check_username("cosita3cz","Github")
 def test_pokeapi():
     costk.PokeAPI.get_pokemon_raw("pikachu")
 def test_main():
-    try:
-        os.system("python cosita_toolkit.py")
-    except Exception as e:
-        print (e)
+    os.system("python cosita_toolkit.py")
