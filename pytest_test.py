fe2ab18d1f7562b0b69a95213d986f8a7a2c7b01
xxcosita3czxx <kokosove18@gmail.com>
2024-05-17 16:01:42 +0200

    error in pytest

--- pytest_test.py
+++ pytest_test.py
@@ -3,7 +3,7 @@ import cosita_toolkit as costk
 
 def test_gitapi_last_info_raw():
     costk.github_api.get_last_info_raw("xxcosita3czxx")
-def test_gitapi_last_info_raw():
+def test_gitapi_last_info_usr():
     costk.github_api.get_info_usr("xxcosita3czxx")
 def test_osint():
     costk.osint_framework.universal.check_username("cosita3cz","Github")
